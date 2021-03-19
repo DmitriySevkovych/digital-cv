28c6ac473bb017510a70c38687066eaf2d329bcd
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-03-19 15:18:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## TODO
+
+- [x] Configure eslint
+- [ ] Configure stylelint
+- [ ] postcss vs sass
+- [x] Configure npm scripts
+- [x] Add font and image Webpack loaders
+- [x] Add glsl and glslify Webpack loaders
+- [x] Configure precommit checks (husky?)
+- [x] Think of how to add sass to project
+- [x] Setup dev Webpack configs
+- [x] Setup prod Webpack configs
+- [x] Adjust Webpack devServer configs
---
+++ bundler/webpack.common.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// This plugin is an alternative to the style-loader, which seems to be better suited for static webpages
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// TODO: work in progress
+// const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/js/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        // publicPath: '/dist',
+        filename: 'js/[name].bundle.js',
+        assetModuleFilename: 'static/[hash][ext][query]'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            // Handlebars
+            {
+                test: /\.hbs$/,
+                use: [
+                    {
+                        loader: 'handlebars-loader',
+                        options: {
+                            // Path to your custom js file, which has Handlebars with custom helpers registered
+                            runtime: path.join(__dirname, 'handlebars.config.js'),
+                            precompileOptions: {
+                                knownHelpersOnly: false,
+                            }
+                        },
+                    }
+                ]
+            },
+            // JS
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/, /extlib/],
+                use: ['babel-loader', 'eslint-loader'],
+                // options: { presets: ['@babel/env'] }
+            },
+            // SASS, CSS
+            {
+                test: /\.(css|s[ac]ss)$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            // Images
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource', // Webpack 5.x: loads file into output folter (file-loader)
+            },
+            {
+                test: /\.(svg)$/i,
+                type: 'asset/source', // Webpack 5.x: loads file content into bundled JS file (raw-loader)
+            },
+            // Fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource', // Webpack 5.x: loads file into output folter (file-loader)
+            },
+            // GLSL
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/i,
+                type: 'asset/source', // Webpack 5.x: loads file content into bundled JS file (raw-loader)
+                exclude: /node_modules/
+            },
+            { test: /\.(glsl|vs|fs|vert|frag)$/i, loader: 'glslify-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        // new StylelintPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/index.hbs'
+        })
+    ]
+}
