6b931db965be3b3c953fd6a23438a06422a2d8ea
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-03-23 21:25:57 +0100

    Start configuring html2canvas

--- src/js/index.js
+++ src/js/index.js
@@ -3,28 +3,38 @@ import '../sass/index.sass';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 
-// TEST jspdf + html2canvas
-const printCV = () => html2canvas(document.querySelector('body')).then(canvas => {
-    //TODO: unclear if necessary
-    canvas.getContext('2d');
-    const imgData = canvas.toDataURL('image/jpeg', 1.0);
+const A4_WIDTH_MM = 297;
+const A4_HEIGHT_MM = 210;
 
-    // Default export is a4 paper, portrait, using millimeters for units
-    const doc = new jsPDF();
+const printCV = () => {
+    html2canvas(
+        document.querySelector('body'),
+        {
+            windowWidth: 1000,
+            // windowHeight: 2000 * A4_WIDTH_MM / A4_HEIGHT_MM
+        }
+    ).then(canvas => {
+        //TODO: unclear if necessary
+        canvas.getContext('2d');
+        const imgData = canvas.toDataURL('image/jpeg', 1.0);
 
-    // Properties
-    doc.setProperties({
-        title: 'CV - Dmitriy Sevkovych',
-        subject: 'This is the subject',
-        author: 'Dmitriy Sevkovych',
-        keywords: 'freelancer, software, mathematics',
-        creator: 'Dmitriy Sevkovych'
+        // Default export is a4 paper, portrait, using millimeters for units
+        const doc = new jsPDF();
+
+        // Properties
+        doc.setProperties({
+            title: 'CV - Dmitriy Sevkovych',
+            subject: 'This is the subject',
+            author: 'Dmitriy Sevkovych',
+            keywords: 'freelancer, software, mathematics',
+            creator: 'Dmitriy Sevkovych'
+        });
+
+        doc.addImage(imgData, 'JPG', 0, 0, A4_WIDTH_MM, A4_HEIGHT_MM);
+
+        doc.save('CV.pdf');
     });
-
-    doc.addImage(imgData, 'JPG', 0, 0, 297, 210);
-
-    doc.save('CV.pdf');
-});
+}
 
 const printButton = document.querySelector('.footer__print');
 printButton.addEventListener('click', printCV);
