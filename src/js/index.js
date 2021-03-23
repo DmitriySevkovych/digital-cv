1fd227ed4a7140c8118fb1a982805494cb5a2ded
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-03-23 12:03:25 +0100

    Testing with jsPDF and html2canvas; I don't like it so far

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import '../sass/index.sass';
+
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+
+// Default export is a4 paper, portrait, using millimeters for units
+const doc = new jsPDF();
+
+// TEST plain jspdf
+// const printableContent = document.querySelector('.body__printable');
+// doc.html(printableContent);
+
+// TEST jspdf + html2canvas
+html2canvas(document.querySelector('.body__printable'))
+    .then(printableContent => doc.html(printableContent));
+
+const printButton = document.querySelector('.footer__print');
+printButton.addEventListener('click', () => doc.save('a4.pdf'));
