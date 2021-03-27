8c201527d8de632c51dd9bbb798e77d87f4e82b2
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-03-27 21:55:32 +0100

    Adjust handlebars config (get JSON context from files); Add more content to page, no styles

--- bundler/handlebars.config.js
+++ bundler/handlebars.config.js
@@ -5,6 +5,11 @@ const layouts = require('handlebars-layouts');
 
 // Register helpers
 handlebars.registerHelper(layouts(handlebars));
+// TODO: adjust helper to load context from a .json (or .yaml) file
+handlebars.registerHelper('getJsonContext', (filepath, options) => {
+    const data = fs.readFileSync(filepath, 'utf8');
+    return options.fn(JSON.parse(data));
+});
 
 // Register partials
 handlebars.registerPartial('layout', fs.readFileSync('src/templates/layouts/layout.hbs', 'utf8'));
