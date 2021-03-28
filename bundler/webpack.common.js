172ad69ec78391e04f6923dbe181578d498cf235
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-03-28 12:20:55 +0200

    Adjust webpack handlebars rule

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
                             precompileOptions: {
                                 knownHelpersOnly: false,
                             },
-                            inlineRequires: '/assets/img/'
+                            inlineRequires: '/assets/*'
                         }
                     }
                 ]
