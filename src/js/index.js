ced9cab26ef91b78beb768ace3e3d08da050c3c2
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-04-18 20:42:10 +0200

    Add card toggling (works with printing as intended); Improve layout (add general A4 padding and more stuff)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 import '../sass/index.sass';
+
+// POC open and close cards aka items
+document.querySelectorAll('.project__item').forEach(item => {
+
+    const itemToggler = item.querySelector('.project__item__head__icon');
+
+    itemToggler.addEventListener('click', () => {
+
+        // Open or close the item if toggler is clicked
+        item.classList.toggle('is_open');
+
+        // The item should only be printed if it is open. Otherwise just hide it.
+        if (item.classList.contains('is_open')) {
+            item.classList.remove('print_not')
+        } else {
+            item.classList.add('print_not')
+        }
+
+    });
+
+});
