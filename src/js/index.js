f2941ade637788f45daa948a8a4efd6e33dafb3f
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-04-28 20:35:53 +0200

    Test neomorphism button in footer; Extract gsap animation into a function

--- src/js/index.js
+++ src/js/index.js
@@ -46,6 +46,18 @@ new Swiper('.swiper-container', {
         delay: 10000,
     },
 
+    breakpoints: {
+        792: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+
+    },
+
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
@@ -62,110 +74,113 @@ lettersElements.forEach(element => {
     element.outerHTML = Splitter(element.outerHTML, '<span class="letter">$</span>');
 });
 
-//create a timeline instance
-const tl = gsap.timeline({ defaults: { ease: 'power.inOut' } });
+const animate = () => {
+    //create a timeline instance
+    const tl = gsap.timeline({ defaults: { ease: 'power.inOut' } });
 
-
-// Searchbar
-tl.from('.header__searchbar', {
-    duration: 0.5,
-    opacity: 0,
-    y: -10
-});
-tl.from('.header__searchbar', {
-    width: 0,
-    duration: 1.5
-}, '+=0.3');
-
-tl.from('.header__searchbar__text .letter',
-    {
+    // Searchbar
+    tl.from('.header__searchbar', {
+        duration: 0.5,
         opacity: 0,
-        stagger: {
-            amount: 3,
+        y: -10
+    });
+    tl.from('.header__searchbar', {
+        width: 0,
+        duration: 1.5
+    }, '+=0.3');
+
+    tl.from('.header__searchbar__text .letter',
+        {
+            opacity: 0,
+            stagger: {
+                amount: 3
+            }
         }
-    }
-);
+    );
 
-// Intro
-tl.from('.header__intro__subtitle',
-    {
-        opacity: 0,
-        duration: 2,
-    }
-);
+    // Intro
+    tl.from('.header__intro__subtitle',
+        {
+            opacity: 0,
+            duration: 2,
+        }
+    );
 
-tl.from('.header__intro__headline', {
-    opacity: 0,
-    duration: 1.5,
-});
-
-
-tl.from('.header__content__picture',
-    {
+    tl.from('.header__intro__headline', {
         opacity: 0,
         duration: 1.5,
-        x: 20,
-        ease: 'power.in'
-    },
-    '-=1'
-);
+    });
 
-tl.from('.header__content__data', { duration: 1.5, opacity: 0, x: -20, ease: 'power.in' }, '-=1.5');
+    tl.from('.header__content__picture',
+        {
+            opacity: 0,
+            duration: 1.5,
+            x: 20,
+            ease: 'power.in'
+        },
+        '-=1'
+    );
 
-// Projects
-tl.from('.career__subtitle.projects__subtitle', { opacity: 0, duration: 0.5 });
+    tl.from('.header__content__data', { duration: 1.5, opacity: 0, x: -20, ease: 'power.in' }, '-=1.5');
 
-tl.from('.project__item',
-    {
-        opacity: 0,
-        y: 10,
-        ease: 'power.inOut',
-        stagger: {
-            each: 0.1
+    // Projects
+    tl.from('.career__subtitle.projects__subtitle', { opacity: 0, duration: 0.5 });
+
+    tl.from('.project__item',
+        {
+            opacity: 0,
+            y: 10,
+            ease: 'power.inOut',
+            stagger: {
+                each: 0.1
+            }
         }
-    }
-);
+    );
 
-// Education
-tl.from('.career__subtitle.education__subtitle', { opacity: 0, duration: 0.5 });
+    // Education
+    tl.from('.career__subtitle.education__subtitle', { opacity: 0, duration: 0.5 });
 
-tl.from('.education__item',
-    {
-        opacity: 0,
-        y: 10,
-        ease: 'power.in',
-        stagger: {
-            each: 0.25
+    tl.from('.education__item',
+        {
+            opacity: 0,
+            y: 10,
+            ease: 'power.in',
+            stagger: {
+                each: 0.25
+            }
         }
-    }
-);
+    );
 
-tl.from('.swiper-button > div',
-    {
-        opacity: 0,
-        y: 10,
-        ease: 'power.in',
-        stagger: {
-            each: 0.25
+    tl.from('.swiper-button > div',
+        {
+            opacity: 0,
+            y: 10,
+            ease: 'power.in',
+            stagger: {
+                each: 0.25
+            }
         }
-    }
-);
+    );
 
-// Coding
-tl.from('.coding__subtitle', { opacity: 0, y: 10, duration: 0.5 });
+    // Coding
+    tl.from('.coding__subtitle', { opacity: 0, y: 10, duration: 0.5 });
 
-tl.from('.coding__overview .gauge',
-    {
-        opacity: 0,
-        y: 10,
-        ease: 'power.in',
-        stagger: {
-            each: 0.1
+    tl.from('.coding__overview .gauge',
+        {
+            opacity: 0,
+            y: 10,
+            ease: 'power.in',
+            stagger: {
+                each: 0.1
+            }
         }
-    }
-);
+    );
 
-tl.from('.coding__message', { opacity: 0, y: 10, duration: 0.5 });
+    tl.from('.coding__message', { opacity: 0, y: 10, duration: 0.5 });
 
-// tl.from('.career', {duration: 1.5, y: -100, opacity: 0});
-tl.from('.A4', { duration: 2, boxShadow: 'none' }, '-=2');
+    // 'Outer' elements
+    tl.from('.A4', { duration: 1, boxShadow: 'none' }, '-=2');
+    tl.from('footer', { duration: 1, opacity: 0, y: 20 }, '-=0.5');
+}
+
+// animate()
