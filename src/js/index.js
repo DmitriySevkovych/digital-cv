28e04fcfb47510da713f740a3f6d2099eb8fbbfe
Ivanna Sevkovych <is@wacg.de>
2021-05-08 20:54:12 +0200

    Hotfix project item collapse issue

--- src/js/index.js
+++ src/js/index.js
@@ -19,9 +19,9 @@ document.querySelectorAll('.project__item').forEach(item => {
 
         // The item should only be printed if it is open. Otherwise just hide it.
         if (item.classList.contains('is_open')) {
-            item.classList.remove('print_not')
+            item.classList.remove('print_not', 'is_collapsed');
         } else {
-            item.classList.add('print_not')
+            item.classList.add('print_not', 'is_collapsed');
         }
 
     });
